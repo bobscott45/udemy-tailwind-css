b3f86b649dfc379e88d8259433944945e7029f08
Robert <bob@rwscott.co.uk>
2021-03-26 11:02:16 +0000

    Define heading font

--- webpack.config.js
+++ webpack.config.js
@@ -3,20 +3,25 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
-module.exports= {
+module.exports = {
     mode: 'development',
     module: {
         rules: [
             {
-              test: /\.js$/,
+                test: /\.js$/,
                 exclude: /node_modules/,
                 use: 'babel-loader'
             },
             {
                 test: /\.css$/,
                 exclude: /node_modules/,
-                use: [ MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader']
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader']
             },
+            {
+                test: /\.ttf$/,
+                exclude: /node_modules/,
+                use:'file-loader'
+            }
         ]
     },
     plugins: [
@@ -25,17 +30,20 @@ module.exports= {
             chunkFilename: "styles.css"
         }),
         new HtmlWebpackPlugin({
-            template:"./src/index.html",
+            template: "./src/index.html",
             filename: "./index.html"
         }),
         new CopyWebpackPlugin({
             patterns: [
-            { from: "./src/img", to: "./img"}],
+                {from: "./src/img", to: "./img"},
+                {from: "./src/fonts", to: "./fonts"}
+            ],
+
         }),
     ],
     devServer: {
         contentBase: path.join(__dirname, 'dist'),
         compress: true,
-        port:9000,
+        port: 9000,
     }
 }
